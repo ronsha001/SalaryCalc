e0dd6206fbc4f07d4fc48cec2229b33cc220d0b7
Ron Sharabi <74726943+ronsha001@users.noreply.github.com>
2021-01-28 23:26:24 +0200

    LastUpdate_28/01/21

--- Calculator_Test.java
+++ Calculator_Test.java
@@ -18,36 +18,41 @@ public class Calculator_Test {
 		String curr = price.format(finSalary);
 		System.out.println("Salary before tax: " + curr);
 
-		int[] arr = { 6290, 9030, 14490, 20140, 41910, 53970, 53971 };
-		double[] precent = { 0.1, 0.14, 0.2, 0.31, 0.35, 0.47, 0.5 };
+		int[] arr = { 0, 6290, 9030, 14490, 20140, 41910, 53970, 53971 };
+		double[] precent = { 1, 0.1, 0.14, 0.2, 0.31, 0.35, 0.47, 0.5 };
 		double[] charge = new double[arr.length];
-		charge[0] = arr[0] * precent[0];
+		// charge[0] = arr[0] * precent[0];
 		for (int i = 1; i < arr.length; i++) {
 			charge[i] = (arr[i] - arr[i - 1]) * precent[i];
 		}
-		charge[arr.length - 1] = (finSalary - arr[arr.length - 1]) * precent[arr.length - 1];
+		charge[arr.length - 1] = (finSalary - arr[arr.length - 1]) * precent[arr.length-1];
+
 		int step = 0;
 		for (int i = 0; i < arr.length; i++) {
 			if (finSalary > arr[i]) {
-				step = i;
+				step = i + 1;
+			} else {
+				break;
 			}
 		}
-		int cut = 0;
-		for (int i = 0; i <= step; i++) {
+		double cut = 0;
+		for (int i = 1; i < step; i++) {
 			cut += charge[i];
 		}
-		if (step < 6 && step != 0) {
-			cut += (finSalary - arr[step]) * precent[step+1];
+		if (step < arr.length - 1) {
+			cut += (finSalary - arr[step - 1]) * precent[step];
 			finSalary -= cut;
-		} else if (step == 0) {
-			cut += (finSalary - arr[step]) * precent[step];
+			curr = price.format(cut);
+			System.out.println("Tax to pay: "+curr);
+			curr = price.format(finSalary);
+			System.out.println("Final salary: "+curr);
 		} else {
 			finSalary -= cut;
-		}
-		curr = price.format(cut);
-		System.out.println("Tax to pay: " + curr);
-		curr = price.format(finSalary);
-		System.out.println("Final salary: " + curr);
+			curr = price.format(cut);
+			System.out.println("Tax to pay: "+curr);
+			curr = price.format(finSalary);
+			System.out.println("Final salary: "+curr);
+		} 
 
 	}
 
